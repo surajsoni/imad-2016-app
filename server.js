b2d264948a6094746edc83a40533c36607ef28c5
surajsoni <sonisuraj261@gmail.com>
2016-09-30 17:49:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-one', function (req, res) {
+  res.send(path.join('artice one is requisted');
+});
+app.get('article-two', function (req, res) {
+  res.send(path.join('artice two is requisted');
+});
+app.get('article-three', function (req, res) {
+  res.send(path.join('artice three is requisted');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
